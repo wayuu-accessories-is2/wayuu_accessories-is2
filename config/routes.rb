ef0ddc0335d7d147489dd81fc4b649428cd1e26b
Daniel Navas <dfnavasm@unal.edu.co>
2016-08-25 23:14:08 -0500

    blank home page setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #get 'home_page/home'
+  root to: 'home_page#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'admin/index', to: 'admin#admin_index'
 end
