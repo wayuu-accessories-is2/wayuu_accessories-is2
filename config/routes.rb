3d7807cfea80d7fefbfe0f11c02e0a33aaaa1398
Daniel Navas <dfnavasm@unal.edu.co>
2016-08-26 02:41:16 -0500

    footer extracted as partial

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'admin/index', to: 'admin#admin_index'
 
-  get 'test', to: 'home_page#home' #Test line [DELETE]
+  get 'home', to: 'home_page#home' #Test line [DELETE]
 end
