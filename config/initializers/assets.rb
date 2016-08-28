18b6aa1d9af923b43a795a6183d9ea60525c13df
Sebastian Moreno <sdmorenob@unal.edu.co>
2016-08-28 14:45:52 -0500

    Final Layout of team page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( hometeam.scss )
 Rails.application.config.assets.precompile += %w( owl.carousel.css )
+Rails.application.config.assets.precompile += %w( hometeam.coffee )
 Rails.application.config.assets.precompile += %w( hometeamscript.js )
 
 # Add additional assets to the asset load path
@@ -13,3 +14,6 @@ Rails.application.config.assets.precompile += %w( hometeamscript.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+#Pero tengo un error, es que no quiero implementar todos los javascripts y css de la página de presentación del equipo en el proyecto final, para ello creé un archivo en la carpeta de los javascripts (intentando copiar la de application) .js y en la carpeta de los stylesheets un archivo .scss (de la misma forma, intentando copiar el application.css) para hacer el require de los archivos que voy a implementar...
+#Sin embargo me aparece que debo
