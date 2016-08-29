776ce2f184c9d0ef5c3eb40f57d0ddf7101eb1d6
Sebastian Moreno <sdmorenob@unal.edu.co>
2016-08-28 20:37:51 -0500

    Final version of team page

--- app/assets/javascripts/hometeam.js
+++
@@ -1,8 +0,0 @@
-//= require easing
-//= require jquery.corner
-//= require jquery.min
-//= require jquery.mixitup.min
-//= require move-top
-//= require owl.carousel
-//= require wow.minS
-//= require hometeamfunctions
