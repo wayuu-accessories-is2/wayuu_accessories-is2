7fd86440edf2c3f7c9d0cc12617ea5ea98820ca7
Trum7 <cadajerp@unal.edu.co>
2016-09-02 18:03:48 -0500

    Precompile fonts.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,16 @@ Rails.application.config.assets.precompile += %w( hometeam.coffee )
 #Rails.application.config.assets.precompile += %w( team_page/hometeamscript.js )
 Rails.application.config.assets.precompile += %w( admin.css )
 Rails.application.config.assets.precompile += %w( admin.coffee )
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.eot )
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.ttf )
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.woff )
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.woff2 )
+Rails.application.config.assets.precompile += %w( FontAwesome.otf )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.eot )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.svg )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.ttf )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.woff )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.woff2 )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
