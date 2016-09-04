924e333b6611215ad89044dc483a0fb5b105e3cb
Daniel Navas <dfnavasm@unal.edu.co>
2016-09-04 01:29:58 +0000

    refactor of some variables

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.precompile += %w( home/home_functions.js )
+Rails.application.config.assets.precompile += %w( store_template/home_page.js )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
