f658a998b5779eb375ab89d0eb3ff4783b46420b
Laura Ceron M <lpceronm@unal.edu.co>
2016-09-06 23:51:11 -0500

    Api basic methods for product

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-
+  namespace :api, defaults:{ format: :json }do
+    namespace :v1 do
+      resources :products, :only => [:show,:index]
+    end
+  end
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions",
