751ead59a95ac3a1777cb4dca8d5947d27de5410
Trum7 <cadajerp@unal.edu.co>
2016-09-06 19:24:55 -0500

    fix heroku nonparallel upload.

--- app/assets/javascripts/admin/product_image.js
+++ app/assets/javascripts/admin/product_image.js
@@ -3,23 +3,34 @@ $(document).on("ready",function(){
     var mediaDropzone = new Dropzone("#image");
     Dropzone.options.mediaDropzone = {
     success: function (response) {
-                eval(response.xhr.response);
+                eval(response.xhr.response),
+                mediaDropzone.processQueue();
             }
     };
     mediaDropzone.options.acceptedFiles = ".jpeg,.jpg,.png,.gif";
     mediaDropzone.options.maxFiles = 7;
     mediaDropzone.options.autoProcessQueue = false;
-    mediaDropzone.options.parallelUploads = 7;
+    mediaDropzone.options.parallelUploads = 1;
     mediaDropzone.on("maxfilesexceeded", function(file) {
         mediaDropzone.removeAllFiles();
         mediaDropzone.addFile(file);
     });
 
-    var submitButton = document.querySelector("#saveimage");
-    submitButton.addEventListener("click", function() {
-         mediaDropzone.processQueue();  // Tell Dropzone to process all queued files.
+    //var submitButton = document.querySelector("#saveimage");
+    //submitButton.addEventListener("click", function() {
+         //mediaDropzone.processQueue();  // Tell Dropzone to process all queued files.
+    //});
+    $(document).on('click', '#saveimage', function () {
+    // enable auto process queue after uploading started
+      mediaDropzone.options.autoProcessQueue = true;
+    // queue processing
+      mediaDropzone.processQueue();
     });
-      $(function() { mediaDropzone.processQueue() });
+
+// disable queue auto processing on upload complete
+    mediaDropzone.on("queuecomplete", function() {
+      mediaDropzone.options.autoProcessQueue = false;
+    });
     var newQueue = [];
     $(function() {
       $("#image").sortable({
