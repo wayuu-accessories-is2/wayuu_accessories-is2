92e5331894bf706d0187d45036e9ae8c164b6b23
Laura Ceron M <lpceronm@unal.edu.co>
2016-09-12 14:43:00 -0500

    Changes on list products, test for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,12 +57,12 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "wayuu_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = {:host => 'wayuumochilabags.herokuapps.com'}
+  config.action_mailer.default_url_options = {:host => 'wayuumochilabags.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
+    domain:               'gmail.com',
     user_name:            ENV["GMAIL_USER"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
