a10779f5cb962b3588c4f74b6857d99a5d3699dc
Laura Ceron M <lpceronm@unal.edu.co>
2016-09-20 18:46:11 -0500

    Revert "Edit sort order for images"
    This reverts commit 99c626550f2958e6319658b65292377558c4259b.

--- app/controllers/product_images_controller.rb
+++ app/controllers/product_images_controller.rb
@@ -1,6 +1,6 @@
 class ProductImagesController < ApplicationController
   before_action :set_images, only: [:show, :edit, :update, :destroy]
-  respond_to :html, :js
+
    # GET /images
    # GET /images.json
   def index
@@ -26,7 +26,7 @@ class ProductImagesController < ApplicationController
    def create
     @image = ProductImage.new(image: params[:file])
     @image.sort_order = session[:order]
-    session[:order] += 1
+    session[:order] += 1 
     @image.product_id = session[:productid]
     @image.save
     #respond_to do |format|
@@ -43,29 +43,16 @@ class ProductImagesController < ApplicationController
 
    # PATCH/PUT /images/1
    # PATCH/PUT /images/1.json
-   def updateorder
-     arreglo = params['info']
-     puts arreglo.size.to_s
-     @update_images = ProductImage.where(:product_id => params['proID'] ).order("sort_order")
-     @update_images.each do |u|
-       for i in (0..arreglo.size-1)
-         if u.sort_order.to_s == (i+1).to_s
-           t_order = arreglo[i].to_i
-           u.sort_order = t_order
-           u.save!
-          break
-         end
-      end
-    end
-    #  respond_to do |format|
-    #    if @image.update(image_params)
-    #      format.html { redirect_to @image, notice: 'Image was successfully updated.' }
-    #      format.json { render :show, status: :ok, location: @image }
-    #    else
-    #      format.html { render :edit }
-    #      format.json { render json: @image.errors, status: :unprocessable_entity }
-    #    end
-    #  end
+   def update
+     respond_to do |format|
+       if @image.update(image_params)
+         format.html { redirect_to @image, notice: 'Image was successfully updated.' }
+         format.json { render :show, status: :ok, location: @image }
+       else
+         format.html { render :edit }
+         format.json { render json: @image.errors, status: :unprocessable_entity }
+       end
+     end
    end
 
    # DELETE /images/1
@@ -83,7 +70,7 @@ class ProductImagesController < ApplicationController
 
      # Never trust parameters from the scary internet, only allow the white list through.
     def images_params
-      params.require(:images).permit(:product_image_id, :image, :sort_order)
+      params.require(:images).permit(:product_image_id, :image)
     end
 
 end
