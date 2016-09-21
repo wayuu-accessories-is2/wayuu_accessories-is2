8e4fbfa12d4705a45e2805e293cfaefb51783e8d
Laura Ceron M <lpceronm@unal.edu.co>
2016-09-20 22:59:57 -0500

    Test new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #stocks = StockStatus.create([{name: 1},{name: 0}])
-category = Category.create([{name: "Solid colors"},{name:"Neon Pink"},{name:"Mini mochilas"}]) 
-administrador = User.create([{email: "administrador@gmail.com", name: "Brandon", role: 53 }])
+category = Category.create([{name: "Solid colors"},{name:"Neon Pink"},{name:"Mini mochilas"},{name:"Black with Color"}])
