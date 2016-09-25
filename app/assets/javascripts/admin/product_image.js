1dd721f4ec45e4002d13b68f9869cac4e3fdfad6
Laura Ceron M <lpceronm@unal.edu.co>
2016-09-24 21:33:44 -0500

    Changes on edit view

--- app/assets/javascripts/admin/product_image.js
+++ app/assets/javascripts/admin/product_image.js
@@ -2,41 +2,34 @@ $(document).on("ready",function(){
   $(document).on("click","#addproduct, #editproduct",function(){
     var mediaDropzone = new Dropzone("#image");
     mediaDropzone.options.acceptedFiles = ".jpeg,.jpg,.png,.gif";
-    mediaDropzone.options.maxFiles = 7;
+    mediaDropzone.options.maxFiles = 12;
     mediaDropzone.options.autoProcessQueue = false;
     mediaDropzone.options.parallelUploads = 1;
 
-    Dropzone.options.mediaDropzone = {
-    success: function (response) {
-                eval(response.xhr.response),
-                mediaDropzone.processQueue();
-            }
-    };
     mediaDropzone.on("addedfile", function(file) {
-
-        var removeButton = Dropzone.createElement('<button class="btn btn-link" >Remove file</button>');
-
-        removeButton.style.cssText = 'position:absolute;left:5px';
-        file.previewElement.appendChild(removeButton);
-        // Capture the Dropzone instance as closure.
-        var _this = this;
-
-        //Listen to the click event
-        removeButton.addEventListener("click", function(e) {
-          // Make sure the button click doesn't submit the form:
-          e.preventDefault();
-          e.stopPropagation();
-          // Remove the file preview.
-          x = confirm("Do you want to delete this file?")
-          if (x){
-            _this.removeFile(file);
-          }else {
-            return false;
-          }
-          // If you want to the delete the file on the server as well,
-          // you can do the AJAX request here.
-        });
+      
+      var removeButton = Dropzone.createElement('<button class="btn btn-link" >Remove file</button>');
+      removeButton.style.cssText = 'position:absolute;left:5px';
+      file.previewElement.appendChild(removeButton);
+      // Capture the Dropzone instance as closure.
+      var _this = this;
+      //Listen to the click event
+      removeButton.addEventListener("click", function(e) {
+        // Make sure the button click doesn't submit the form:
+        e.preventDefault();
+        e.stopPropagation();
+        // Remove the file preview.
+        x = confirm("Do you want to delete this file?")
+        if (x){
+          _this.removeFile(file);
+        }else {
+          return false;
+        }
+        // If you want to the delete the file on the server as well,
+        // you can do the AJAX request here.
       });
+    });
+
     mediaDropzone.on("maxfilesexceeded", function(file) {
         mediaDropzone.removeAllFiles();
         mediaDropzone.addFile(file);
@@ -47,18 +40,19 @@ $(document).on("ready",function(){
       mediaDropzone.options.autoProcessQueue = true;
     // queue processing
       mediaDropzone.processQueue();
-
+      mediaDropzone.on("queuecomplete", function() {
+        // disable queue auto processing on upload complete
+        mediaDropzone.options.autoProcessQueue = false;
+        // Redirect page to index
+        //window.location.reload();
+        $.ajax({
+          url: '/admin/index',
+          type: 'GET',
+        });
+      });
     });
 
-// disable queue auto processing on upload complete
-    mediaDropzone.on("queuecomplete", function() {
-      mediaDropzone.options.autoProcessQueue = false;
-      $.ajax({
-  			url: '/admin/index',
-  			type: 'GET',		
-  		});
 
-    });
     var newQueue = [];
     $(function() {
       $("#image").sortable({
@@ -87,3 +81,10 @@ $(document).on("ready",function(){
 
   });
 });
+
+// Dropzone.options.mediaDropzone = {
+// success: function (response) {
+//             eval(response.xhr.response),
+//             mediaDropzone.processQueue();
+//         }
+// };
