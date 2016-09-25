c71e978357d0809fc36d03644c830e786ead9ec1
Laura Ceron M <lpceronm@unal.edu.co>
2016-09-25 11:11:11 -0500

    Initial set up for i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,15 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
   protect_from_forgery with: :null_session, prepend: true
   helper_method :current_user
+  before_action :set_locale
+
+
 
   private
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
+    
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
