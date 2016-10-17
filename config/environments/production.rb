071ede55a0c9d3c0813031758b53d6799755e981
Cristian Orjuela <ccorjuelav@unal.edu.co>
2016-10-16 23:32:06 -0500

    Change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,13 +58,13 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {host: 'pruebawayuuteam.herokuapp.com',}
+  config.action_mailer.default_url_options = {host: 'wayuumochilabags.herokuapp.com',}
   ActionMailer::Base.smtp_settings = {
-    :address    => 'smtp.sendgrid.net',
+    :address    => 'smtp.gmail.com',
     :port       => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['GMAIL_USERNAME'],
+    :password       => ENV['GMAIL_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
