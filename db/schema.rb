86d247bc4e052ec2bdaa0498beffbbba0c44a31b
Cristian Orjuela <ccorjuelav@unal.edu.co>
2016-10-17 19:20:26 -0500

    A new change

--- db/schema.rb
+++ db/schema.rb
@@ -211,7 +211,7 @@ ActiveRecord::Schema.define(version: 20160908000703) do
     t.string   "name"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
-    t.integer  "role"
+    t.integer  "role", default: 7
     t.string   "auth_token"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
