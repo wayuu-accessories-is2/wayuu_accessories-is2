df32551baf16ebea9fb63db8ae54ef4a2dca1e9d
Trum7 <cadajerp@unal.edu.co>
2016-10-19 01:39:50 -0500

    payment views implementation.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,15 @@
 class CheckoutController < ApplicationController
 
+  TRANSACTION_SUCCESS_STATUSES = [
+    Braintree::Transaction::Status::Authorizing,
+    Braintree::Transaction::Status::Authorized,
+    Braintree::Transaction::Status::Settled,
+    Braintree::Transaction::Status::SettlementConfirmed,
+    Braintree::Transaction::Status::SettlementPending,
+    Braintree::Transaction::Status::Settling,
+    Braintree::Transaction::Status::SubmittedForSettlement,
+  ]
+
   respond_to :html, :js
 
   def index
@@ -47,10 +57,9 @@ class CheckoutController < ApplicationController
   end
 
   def second
-
-    respond_to do |format|
-      format.js
-    end
+    @valor = 99.9
+    @client_token = Braintree::ClientToken.generate
+    render layout: "payment"
 
   end
 
@@ -91,6 +100,56 @@ class CheckoutController < ApplicationController
       end
     end
     return @sum/2
+  end
+
+
+
+  def new
+    @client_token = Braintree::ClientToken.generate
+  end
+
+  def show
+    @transaction = Braintree::Transaction.find(params[:id])
+    @result = _create_result_hash(@transaction)
+  end
+
+  def create
+    amount = params["amount"] # In production you should not take amounts directly from clients
+    nonce = params["payment_method_nonce"]
+
+    result = Braintree::Transaction.sale(
+      amount: amount,
+      payment_method_nonce: nonce,
+      :options => {
+        :submit_for_settlement => true
+      }
+    )
+
+    if result.success? || result.transaction
+      redirect_to checkout_path(result.transaction.id)
+    else
+      error_messages = result.errors.map { |error| "Error: #{error.code}: #{error.message}" }
+      flash[:error] = error_messages
+      redirect_to new_checkout_path
+    end
+  end
+
+  def _create_result_hash(transaction)
+    status = transaction.status
+
+    if TRANSACTION_SUCCESS_STATUSES.include? status
+      result_hash = {
+        :header => "Sweet Success!",
+        :icon => "success",
+        :message => "Your transaction has been successfully processed."
+      }
+    else
+      result_hash = {
+        :header => "Transaction Failed",
+        :icon => "fail",
+        :message => "Your test transaction has a status of #{status}."
+      }
+    end
   end
 
 end
