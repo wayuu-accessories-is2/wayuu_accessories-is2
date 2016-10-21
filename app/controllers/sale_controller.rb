83c5c452f9eb60fd4f53e95bb934d58513f3501f
Trum7 <cadajerp@unal.edu.co>
2016-10-20 21:43:40 -0500

    Order information.

--- app/controllers/sale_controller.rb
+++ app/controllers/sale_controller.rb
@@ -15,6 +15,16 @@ class SaleController < ApplicationController
 	def details
 		details = params[:id]
 		@detailorder = Order.find( details )
+		@deliverycustomer = Customer.find( @detailorder.customer_id )
+		@deliveryaddress = Address.find( @detailorder.address_id )
+
+		@transaction = CustomerTransaction.where("order_id = ?", @detailorder.id ).first
+		puts @transaction.inspect
+		@billingcustomer = Customer.find( @transaction.customer_id )
+		puts "pailas"
+		@billingaddress = Address.find( @transaction.address_id )
+
+
 		@productsorder = OrderProduct.where("order_id = ?", @detailorder.id )
 
 	end
