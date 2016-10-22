03275e4f20e60da997ad275dc21c3dc6b7b003af
Sebastian D. Moreno B <sdmorenob@unal.edu.co>
2016-10-22 01:07:01 -0500

    Hotfix with last merge

--- db/schema.rb
+++ db/schema.rb
@@ -39,13 +39,6 @@ ActiveRecord::Schema.define(version: 20161019211207) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "carts", force: :cascade do |t|
-    t.integer  "customer_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["customer_id"], name: "index_carts_on_customer_id", using: :btree
-  end
-
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.integer  "status",     default: 1
