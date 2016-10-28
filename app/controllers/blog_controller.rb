e88f6985c24467f549cc15254d912fb5be2de5d6
Sebastian D. Moreno B <sdmorenob@unal.edu.co>
2016-10-28 14:29:29 -0500

    Generate view to reply of comments

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -8,5 +8,11 @@ class BlogController < ApplicationController
     @article = Article.find_by( id: params[:id] )
     @name_blog = @article.title
     @comments = BlogComment.where(article_id: params[:id]).order('created_at DESC')
+    @replies = []
+    @comments.each do |t|
+      replies = BlogCommentReply.where(blog_comment_id: t.id)
+      @replies += replies
+    end
+
   end
 end
